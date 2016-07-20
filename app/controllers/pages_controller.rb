86a0a8b81aa682d15b967cb6abf6c4c9767eb472
Mark Richardson <markeban@yahoo.com>
2016-07-19 18:59:03 -0700

    made it dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,19 @@
 class PagesController < ApplicationController
 
+  def home
+    @current_time = Time.now.strftime("%a, %e %b %Y %H:%M:%S %z")
+  end
+
   def about
-    # talk to a database
+    number_1 = 4
+    number_2 = 8
+    @sum = number_1 + number_2
   end
 
   def contact_us
-    
+    @pets = ["fish", "cat", "catdog"]
+    puts "here's my pets"
+    p @pets
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'pages#home'
   get '/about', to: 'pages#about'
   get '/contact_us', to: 'pages#contact_us'
 end
